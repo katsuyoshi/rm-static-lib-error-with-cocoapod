56833b4219d3369a684d959aa25013ee0fb9e56f
Katsuyoshi Ito <kito@itosoft.com>
2019-06-20 06:40:39 +0900

    initial import

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'rake'
+# Add your dependencies here:
+
+gem 'motion-cocoapods'#, :git => 'https://github.com/HipByte/motion-cocoapods.git' #, '1.8.0'
---
+++ app/foo.rb
@@ -0,0 +1,11 @@
+class Foo
+  def foo
+    NSLog "This is the #foo method written in RubyMotion"
+  end
+end
+
+class NSString
+  def bar
+    NSLog "This is the #bar added to NSString extension method, written in RubyMotion"
+  end
+end
